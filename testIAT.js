a922e5426892b8541ddf9c5c5bf6fc95645030fc
shodout <119656222+shodout@users.noreply.github.com>
2022-12-06 11:27:24 +0800

    Create testIAT.js

---
+++ testIAT.js
@@ -0,0 +1,45 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Minorities', //Will appear in the data.
+			title : {
+				media : {word : 'Minorities'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'Minorities1.jpg'}, 
+    			{image : 'Minorities2.jpg'}, 
+    			{image : 'Minorities3.jpg'}, 
+    			{image : 'Minorities4.jpg'}, 
+    			{image : 'Minorities5.jpg'}, 
+    			{image : 'Minorities6.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Chinese', //Will appear in the data.
+			title : {
+				media : {word : 'Chinese'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'Chinese1.jpg'}, 
+    			{image : 'Chinese2.jpg'}, 
+    			{image : 'Chinese3.jpg'}, 
+    			{image : 'Chinese4.jpg'}, 
+    			{image : 'Chinese5.jpg'}, 
+    			{image : 'Chinese6.jpg'}			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://shodout.github.io/IAT/images/'
+		} 
+	});
+});
