df74364cacf9772d501997fbeb0e158dd97f9349
shodout <119656222+shodout@users.noreply.github.com>
2022-12-08 09:05:45 +0800

    Update and rename testIATcom.js to gendercareerIATcom.js

--- gendercareerIATcom.js
+++ gendercareerIATcom.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://shodout.github.io/IAT/testAPIcom.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://shodout.github.io/IAT/gendercareerAPIcom.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
