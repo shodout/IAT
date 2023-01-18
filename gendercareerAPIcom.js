8c5229f81d457e40f5bf5784e8a28eb50c9a7985
shodout <119656222+shodout@users.noreply.github.com>
2023-01-18 09:56:01 +0800

    Update gendercareerAPIcom.js

--- gendercareerAPIcom.js
+++ gendercareerAPIcom.js
@@ -1300,7 +1300,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
                 {
 					inherit : 'Default',
                     media : {word : (isTouch ? piCurrent.debriefingTextBottomTouch:piCurrent.debriefingTextBottom)},
-                    location:{left:2,top:45,right:2}
+                    location:{left:2,top:55,right:2}
                 }
              
                 
